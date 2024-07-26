59c57766c82b37a1e99919616a5f696478cb5f33
HuiDi Hu <andyhhdi@gmail.com>
2024-07-25 21:04:20 -0700

    ChannelHeader with friendsIcon. SQL queries layout
    Co-authored-by: Xinrui Chen <globalcooling1@users.noreply.github.com>
    Co-authored-by: Brandon <BrandonVH@users.noreply.github.com>

---
+++ backend/layout/Servers.sql
@@ -0,0 +1,26 @@
+/*TODO 
+(upon user login)
+1. Find User And Record Information
+2. Load Servers
+3. Load Friend List
+4. Load Dms
+
+(upon clicking server)
+1. Load Channels
+2. Load Server Members
+3. Load Messages Of First Channel
+
+(upon clicking channel)
+1. Load Messages Of Clicked Channel
+
+(upon clicking dm or user in friend list)
+1. Load Messages Of Dm
+2. Load 2 Users Of Dm
+*/
+
+--load all the servers the user is a part of upon their login
+SELECT 
+    *
+FROM 
+    SERVERS 
+WHERE
