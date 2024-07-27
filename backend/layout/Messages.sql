5b14ddb0f106cd701a33cd099ff3920ee224ef8c
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-07-26 18:20:28 -0700

    Update Messages.sql

--- backend/layout/Messages.sql
+++ backend/layout/Messages.sql
@@ -18,6 +18,7 @@ CREATE TABLE DMS(
 CREATE TABLE CHANNEL_MESSAGES(
   message_id SERIAL PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /*includes time value unlike DATE*/
+  is_edited INTEGER DEFAULT 0,
   content VARCHAR(10000) NOT NULL,
   posted_by VARCHAR NOT NULL, /*holds userid*/
   in_channel INTEGER NOT NULL, /*holds channel_id*/
@@ -27,6 +28,7 @@ CREATE TABLE CHANNEL_MESSAGES(
 CREATE TABLE DM_MESSAGES(
   message_id SERIAL PRIMARY KEY,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /*includes time value unlike DATE*/
+  is_edited INTEGER DEFAULT 0,
   content VARCHAR(10000) NOT NULL,
   posted_by VARCHAR NOT NULL, /*holds userid*/
   in_dm INTEGER NOT NULL /*holds channel_id*/
@@ -53,7 +55,8 @@ WHERE
 UPDATE 
   DM_MESSAGES
 SET 
-  content = <updated content>
+  content = <updated content>,
+  is_edited = 1
 WHERE 
   message_id = <id of message being updated>
 
@@ -61,7 +64,8 @@ WHERE
 UPDATE 
   CHANNEL_MESSAGES
 SET 
-  content = <updated content>
+  content = <updated content>,
+  is_edited = 1
 WHERE 
   message_id = <id of message being updated>
 
