3952f0e1968e59784acd2dd3ba5c7268057dc634
Brandon <167170288+BrandonVH@users.noreply.github.com>
2024-07-26 18:22:54 -0700

    Update Messages.sql

--- backend/layout/Messages.sql
+++ backend/layout/Messages.sql
@@ -12,8 +12,8 @@ CREATE TABLE DMS(
   /*
   user1_id INTEGER NOT NULL, /*combine with user2_id to make array?*/ --sure! make sure both are not NULL
   user2_id INTEGER NOT NULL
+  */
 );
-*/
 
 CREATE TABLE CHANNEL_MESSAGES(
   message_id SERIAL PRIMARY KEY,
