61a05e93ee3998c0c50727adc267cf235db84ad5
HuiDi Hu <andyhhdi@gmail.com>
2024-07-29 10:17:05 -0700

    switch hover state variable into group-hover

--- frontend/src/components/channels/ServerSideNavBar.jsx
+++ frontend/src/components/channels/ServerSideNavBar.jsx
@@ -10,21 +10,18 @@ import { ServerContext } from 'src/pages/Channels';
 
 const ServerSideNavBar = ({ props }) => {
     const { serverList } = useContext(ServerContext)
-    const [hoveredPath, setHoveredPath] = useState("")
     const [isAddServerOpen, setIsAddServerOpen] = useState(false)
     const navigate = useNavigate();
     return (
         <div className="relative min-w-fit flex flex-col overflow-y-scroll py-5 pr-2 space-y-2 items-center scrollbar-hide">
-            <div className="flex items-center space-x-2">
+            <div className="group/home flex items-center space-x-2">
                 <span
-                    className={`${props.selectedPath.startsWith('/@me') ? 'h-8' : (hoveredPath.startsWith('/@me') ? 'h-4' : 'h-1')} 
+                    className={`${props.selectedPath.startsWith('/@me') ? 'h-8' : 'h-1 group-hover/home:h-4'} 
                                     border-2 rounded-tr-xl rounded-br-xl transition-all ease-in-out duration-300`}
                 />
                 <div
                     className={`flex justify-center items-center h-12 w-12
-                             ${(props.selectedPath.startsWith('/@me') || hoveredPath.startsWith('/@me')) ? "rounded-2xl bg-[#313167]" : "rounded-[50%] bg-[#31313c]"} transition-all duration-300 ease-in-out`}
-                    onMouseOver={() => { setHoveredPath('/@me') }}
-                    onMouseOutCapture={() => { setHoveredPath('') }}
+                             ${props.selectedPath.startsWith('/@me') ? "rounded-2xl bg-[#313167]" : "rounded-[50%] bg-[#31313c]"} group-hover/home:rounded-2xl group-hover/home:bg-[#313167] transition-all duration-300 ease-in-out`}
                     onClick={() => {
                         props.setSelectedPath('/@me')
                         navigate('/channels/@me')
@@ -39,26 +36,24 @@ const ServerSideNavBar = ({ props }) => {
             <div className="w-full flex justify-end pr-2"><span className="border w-8"></span></div>
             {serverList.map(item => (
                 <SideNavItem key={`sv:${item.server_id}`} props={{
-                    selectedPath: props.selectedPath, setSelectedPath: props.setSelectedPath, hoveredPath, setHoveredPath,
+                    selectedPath: props.selectedPath, setSelectedPath: props.setSelectedPath,
                     server_id: item.server_id,
                     icon: item.server_icon || 'GRAGAS',
                     server_name: item.server_name
                 }}
                 />
             ))}
-            <div className="flex items-center space-x-2">
+            <div className="group/add flex items-center space-x-2">
                 <span
-                    className={`${isAddServerOpen ? 'h-8' : (hoveredPath.startsWith('/add_server') ? 'h-4' : 'h-1 border-transparent')} 
+                    className={`${isAddServerOpen ? 'h-8 border-white' : 'group-hover/add:h-4 group-hover/add:border-white h-1 border-transparent'} 
                                     border-2 rounded-tr-xl rounded-br-xl transition-all ease-in-out duration-300`}
                 />
                 <div
                     className={`flex justify-center items-center h-12 w-12
-                             ${(isAddServerOpen || hoveredPath.startsWith('/add_server')) ? "rounded-2xl bg-green-500" : "rounded-[50%] bg-[#31313c]"} transition-all duration-300 ease-in-out`}
-                    onMouseOver={() => { setHoveredPath('/add_server') }}
-                    onMouseOutCapture={() => { setHoveredPath('') }}
+                             ${isAddServerOpen ? "rounded-2xl bg-green-500" : "group-hover/add:rounded-2xl group-hover/add:bg-green-500 rounded-[50%] bg-[#31313c]"} transition-all duration-300 ease-in-out`}
                     onClick={() => { setIsAddServerOpen(true) }}
                 >
-                    <GoPlus className={`h-8 w-8 ${(isAddServerOpen || hoveredPath.startsWith('/add_server')) ? "text-white" : "text-green-500"}`} />
+                    <GoPlus className={`h-8 w-8 ${isAddServerOpen ? "text-white" : "group-hover/add:text-white text-green-500"}`} />
                 </div>
             </div>
 
--- frontend/src/components/channels/SideNavItem.jsx
+++ frontend/src/components/channels/SideNavItem.jsx
@@ -7,26 +7,24 @@ const SideNavItem = ({ props }) => {
     const { setMsgLoading, setSidebarLoading } = useContext(LoadingContext)
     const navigate = useNavigate();
     return (
-        <div className="relative flex items-center space-x-2">
+        <div className="group relative flex items-center space-x-2">
             <span
-                className={`${props.selectedPath === `/server/${props.server_id}` ? 'h-8' : (props.hoveredPath === `/server/${props.server_id}` ? 'h-4' : 'h-1')} 
+                className={`${props.selectedPath === `/server/${props.server_id}` ? 'h-8' : 'h-1 group-hover:h-4' } 
                                     border-2 rounded-tr-xl rounded-br-xl transition-all ease-in-out duration-300`}
             />
             <img
                 className={`flex justify-center items-center h-12 w-12 m-auto
-                             ${(props.selectedPath === `/server/${props.server_id}` || props.hoveredPath === `/server/${props.server_id}`) ? "rounded-2xl" : "rounded-[50%]"} transition-[border-radius] duration-300 ease-in-out`}
+                             ${props.selectedPath === `/server/${props.server_id}` ? "rounded-2xl" : "rounded-[50%]"} group-hover:rounded-2xl transition-[border-radius] duration-300 ease-in-out`}
                 src={`../../../assets/tempIcons/${props.icon}.png`}
-                onMouseOver={() => { props.setHoveredPath(`/server/${props.server_id}`) }}
-                onMouseOutCapture={() => { props.setHoveredPath('') }}
                 onClick={() => { 
                     props.setSelectedPath(`/server/${props.server_id}`);
                     setMsgLoading(true); setSidebarLoading(true);
                     navigate(`/channels/server/${props.server_id}`)
                 }}
             />
-            {props.hoveredPath === `/server/${props.server_id}` && <div className='fixed left-16 max-w-44 max-h-16 z-10 rounded-lg bg-black py-2 px-4'>
+            <div className='hidden group-hover:block fixed left-16 max-w-44 max-h-16 z-10 rounded-lg bg-black py-2 px-4'>
                 <p className='text-ellipsis overflow-hidden text-md'>{props.server_name}</p>
-            </div>}
+            </div>
         </div>
     )
 }
