a1c523ab62a598d7bd0b08761a96a2a30fd5e11e
HuiDi Hu <andyhhdi@gmail.com>
2024-08-12 01:26:09 -0700

    redis config

--- backend/redis.js
+++ backend/redis.js
@@ -1,3 +1,6 @@
 const Redis = require('ioredis')
-const redisClient = new Redis()
+const redisClient = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT || 6379
+});
 module.exports = redisClient;
