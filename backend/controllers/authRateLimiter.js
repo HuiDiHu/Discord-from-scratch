2e1d821d681a96520215389dfe59cf7465a93875
HuiDi Hu <andyhhdi@gmail.com>
2024-08-11 21:30:25 -0700

    browserRouter, optional rate limitor

--- backend/controllers/authRateLimiter.js
+++ backend/controllers/authRateLimiter.js
@@ -6,6 +6,10 @@ const authRateLimiter = (limit, expirationTime) =>
 async (req, res, next) => {
     //
     const ipAddr = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    if (!ipAddr) {
+        next();
+        return;
+    }
     const [response] = await redisClient.multi().incr(ipAddr).expire(ipAddr, expirationTime).exec() //# of auth attemps from a single user
     if (response[1] > limit) {
         res.status(StatusCodes.BAD_REQUEST).json({ loggedIn: false, emailErrMsg: `Slow down! Try again in ${expirationTime} seconds.`, passwordErrMsg: " " })
